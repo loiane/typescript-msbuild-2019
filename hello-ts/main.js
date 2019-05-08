8542ed0aef3d209c987d939cab8b95349609d312
Loiane <loianeg@gmail.com>
2019-05-08 11:39:18 -0400

    main.js

--- hello-ts/main.js
+++ hello-ts/main.js
@@ -1,3 +1,10 @@
-var hello = 'Build 2019';
+let hello = 'Build 2019';
 hello = 3;
 console.log(hello);
+
+class Attendee {
+    constructor(name, city) {
+        this._name = name;
+        this._city = city;
+    }
+}
